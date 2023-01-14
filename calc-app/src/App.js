16989eee7503e8964f3602f699fff0e106ed284a
14harish <harishpc2003@gmail.com>
2023-01-14 20:26:33 +0530

    new

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -5,27 +5,27 @@ function App() {
   return (
     <div className="App">
       <input type="text" placeholder="0" id="answer"/>
-      <input type="button" value="9" className='button'/>
+      <div className='Number'>
+      <input type="button" value="AC" className='button gr'/>
+      <input type="button" value="." className='button gr'/>
+      <input type="button" value="%" className='button gr'/>
+      <input type="button" value="/" className='button yello'/>
+      <input type="button" value="9" className='button '/>
       <input type="button" value="8" className='button'/>
       <input type="button" value="7" className='button'/>
+      <input type="button" value="*" className='button yello'/>
       <input type="button" value="6" className='button'/>
       <input type="button" value="5" className='button'/>
       <input type="button" value="4" className='button'/>
+      <input type="button" value="-" className='button yello' />
       <input type="button" value="3" className='button'/>
       <input type="button" value="2" className='button'/>
       <input type="button" value="1" className='button'/>
+      <input type="button" value="+" className='button yello'/>
 
-      <input type="button" value="0" className='button'/>
-      <input type="button" value="+" className='button'/>
-      <input type="button" value="-" className='button'/>
-      <input type="button" value="/" className='button'/>
-      <input type="button" value="%" className='button'/>
-      <input type="button" value="*" className='button'/>
-      <input type="button" value="%" className='button'/>
-      <input type="button" value="." className='button'/>
-
-      <input type="button" value="clear" className='button button1'/>
-      <input type="button" value="=" className='button button1'/>
+      <input type="button" value="0" className='button button1'/>
+      <input type="button" value="=" className='button yello'/>
+      </div>
 
     </div>
   );
