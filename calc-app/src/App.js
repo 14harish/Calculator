9751670724270037e6d7ddf9c5427b9b948ddddd
14harish <harishpc2003@gmail.com>
2023-01-14 19:11:16 +0530

    new

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="0" id="answer"/>
+      <input type="button" value="9" className='button'/>
+      <input type="button" value="8" className='button'/>
+      <input type="button" value="7" className='button'/>
+      <input type="button" value="6" className='button'/>
+      <input type="button" value="5" className='button'/>
+      <input type="button" value="4" className='button'/>
+      <input type="button" value="3" className='button'/>
+      <input type="button" value="2" className='button'/>
+      <input type="button" value="1" className='button'/>
+
+      <input type="button" value="0" className='button'/>
+      <input type="button" value="+" className='button'/>
+      <input type="button" value="-" className='button'/>
+      <input type="button" value="/" className='button'/>
+      <input type="button" value="%" className='button'/>
+      <input type="button" value="*" className='button'/>
+      <input type="button" value="%" className='button'/>
+      <input type="button" value="." className='button'/>
+
+      <input type="button" value="clear" className='button button1'/>
+      <input type="button" value="=" className='button button1'/>
+
     </div>
   );
 }
